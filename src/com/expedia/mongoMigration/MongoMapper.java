8f757a11173e86e8ee1197c5a2ea62e9b613e99d
Zaal Tonia <ztonia@launchcg.com>
2015-11-02 18:07:44 -0800

    Wrapping draft files to allow for compile

--- src/com/expedia/mongoMigration/MongoMapper.java
+++ src/com/expedia/mongoMigration/MongoMapper.java
@@ -1,11 +1,17 @@
-Morphia morphia = new Morphia();
-morphia.map(Check.class);
-
-/* You can reuse this datastore */
-Datastore datastore = morphia.createDatastore(mongoClient, "myDatabase");
-
-/* 
- * Jackson's ObjectMapper, which is reusable, too,
- * does all the magic.
- */
-ObjectMapper mapper = new ObjectMapper();
+//import org.mongodb.morphia;
+//import com.expedia.importCheck.check;
+//
+//public class mMapper {
+//    Morphia morphia = new Morphia();
+//    morphia.map(Check.class);
+//
+//    /* You can reuse this datastore */
+//    Datastore datastore = morphia.createDatastore(mongoClient, "myDatabase");
+//
+//    /* 
+//     * Jackson's ObjectMapper, which is reusable, too,
+//     * does all the magic.
+//     */
+//    ObjectMapper mapper = new ObjectMapper();
+//
+//}
